dd01904b44fa73a4b1b610c428cb4aa2ca1622a6
Firat <Firat_asi@icloud.com>
2025-01-18 02:42:32 +0300

    exc3

---
+++ Methods/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+        }
+    }
+
+
+    class  Matematik
+    {
+        public void Topla(int a, int b)
+        {
+            int c = a + b;
+            Console.WriteLine(c);
+        }
+
+        public void Cikar(int a, int b)
+        {
+            int c = a - b;
+            Console.WriteLine(c);
+        }
+
+        public void Carp(int a, int b)
+        {
+            int c = a * b;
+            Console.WriteLine(c);
+        }
+
+        public void Bol(int a, int b)
+        {
+            int c = a / b;
+            Console.WriteLine(c);
+        }
+
+
+    }
+
+
+}
