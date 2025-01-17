797cccc603ed37f790b63cab9a96e8d9f2752f8b
Firat <Firat_asi@icloud.com>
2025-01-17 23:11:56 +0300

    exc

---
+++ Classes/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Classes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Student student = new Student();
+            student.FirstName = "Firat";
+            student.Age = 1;
+            student.Mail = "asd@gmail.com";
+
+            Student student1 = new Student();
+            student.FirstName = "Boran";
+            student.Age = 1;
+            student.Mail = "asd@gmail.com";
+
+            Student student2 = new Student();
+            student.FirstName = "Ali";
+            student.Age = 1;
+            student.Mail = "asd@gmail.com";
+
+            List<Student> students = new List<Student>() {student, student1, student2 };
+
+            foreach (var student in students)
+            {
+                MessageBox.Show(student.FirstName());
+            }
+        }
+    }
+}
---
+++ Classes/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DocuSign.eSign.Model;
+
+namespace Classes
+{
+    class Student
+    {
+        public string MyProperty FirstName { get; set; }
+        public int String Age { get; set;} 
+        public String Mail { get; set;}
+    }
+}
+
+
